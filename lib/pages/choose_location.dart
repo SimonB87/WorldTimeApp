0a4fce1d2f7cc41356a538c5d5c2b607b928daa2
SimonB87 <buryans@gmail.com>
2020-12-19 16:50:20 +0100

    Lesson 26 - http package, http requests

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,39 +7,14 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-
-    // simulate network request for a username
-
-    String userName = await Future.delayed(
-        Duration(seconds: 3),
-        () {
-            return "Yoshi";
-    });
-    String bio = await Future.delayed(
-        Duration(seconds: 2),
-        () {
-          return "Yoshi bio";
-        });
-    print("$userName - $bio");
-  }
-
-  int counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
-    print("built function ran");
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
         title: Text("Choose a location"),
         centerTitle: true,
+        elevation: 0,
       ),
       body: Text("Hello there"),
     );

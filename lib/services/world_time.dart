163bb8fecc6268863e5ac2d86f640968c7e879b3
SimonB87 <buryans@gmail.com>
2020-12-20 14:59:58 +0100

    Lesson 29 - Error handling: try + catch

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,39 +11,45 @@ class WorldTime{
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    // make the request
-    //Response response = await get('http://worldtimeapi.org/api/timezone/Europe/London');
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    //print(data);
 
-    // get properties from json
-    String datetime = data['datetime'];
-    String offsetOriginal = data['utc_offset'].substring(2,3);
-    String offset = offsetOriginal;
+    try {
+      // make the request
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url'); //'http://worldtimeapi.org/api/timezone/Europe/London'
+      Map data = jsonDecode(response.body);
+      //print(data);
 
-    //edit offset value
-    String offsetOver10 = data['utc_offset'].substring(1,2);
-    if (offsetOver10 == "1") {
-      offset = "1" + offset;
+      // get properties from json
+      String datetime = data['datetime'];
+      String offsetOriginal = data['utc_offset'].substring(2,3);
+      String offset = offsetOriginal;
+
+      //edit offset value
+      String offsetOver10 = data['utc_offset'].substring(1,2);
+      if (offsetOver10 == "1") {
+        offset = "1" + offset;
+      }
+      String offsetSign = data['utc_offset'].substring(0,1);
+      if (offsetSign == "-") {
+        offset = "-" + offset;
+      }
+
+      //print(datetime);
+      //print(offset);
+
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      if ( offsetOriginal != "0" ) {
+        now = now.add(Duration(hours: int.parse(offset)));
+      }
+
+      // set the time property
+      time = now.toString();
+      //print(time);
     }
-    String offsetSign = data['utc_offset'].substring(0,1);
-    if (offsetSign == "-") {
-      offset = "-" + offset;
+    catch (e) {
+      print("caught error: $e");
+      time = "could not get data";
     }
-
-    //print(datetime);
-    //print(offset);
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    if ( offsetOriginal != "0" ) {
-      now = now.add(Duration(hours: int.parse(offset)));
-    }
-
-    // set the time property
-    time = now.toString();
-    //print(time);
   }
 
 }
